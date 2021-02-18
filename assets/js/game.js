d585eee01712c6565230369b2e8875d44eb86467
Duality <jramirezarroyo21@gmail.com>
2021-02-17 19:12:28 -0800

    initial game setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,7 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
@@ -11,8 +12,14 @@ var enemyAttack = 12;
 
 var fight = function() {
   window.alert("Welcome to Robot Gladiators!");
+
+  var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+
+  console.log(promptFight);
   
   // Dubtract the value of playerAttack from the value of enemyHealth and use that result to update enemyHealth
+
+if(promptFight == "fight" || promptFight == "FIGHT") {
 
   enemyHealth = enemyHealth - playerAttack;
   
@@ -28,7 +35,6 @@ var fight = function() {
    else {
     window.alert(enemyName + " still has " + enemyHealth + " health left.");
   }
-
   // Subtract the value of enemyAttack from the value of playerHealth and use that result to update playerHealth
       
   playerHealth = playerHealth - enemyAttack;
@@ -45,6 +51,22 @@ var fight = function() {
   } 
   else {
     window.alert(playerName + " still has " + playerHealth + " health left.");
+  }
+ // SKIPPING fight
+} else if (promptFight === "skip" || promptFight === "SKIP") {
+    // confirm player wants to skip
+    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+  
+    // if yes (true), leave fight
+    if (confirmSkip) {
+      window.alert(playerName + " has decided to skip this fight. Goodbye!");
+      // subtract money from playerMoney for skipping
+      playerMoney = playerMoney - 2;
+    }
+    // if no (false), ask question again by running fight() again
+    else {
+      fight();
+    }
   }
 
 };
