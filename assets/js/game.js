976f917135ae5b9ff92659c1f3cf3917391ccd6d
Duality <jramirezarroyo21@gmail.com>
2021-02-17 17:01:39 -0800

    Console, Variables, Fucntions, &String Concatenation

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,16 @@
+
+// asks player for their robot's name
+
+var playerName = window.prompt("What is your robot's name?");
+
+console.log("Our robot's name is " + playerName);
+
 window.alert("This is an alert! JavaScript is running!");
+
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
